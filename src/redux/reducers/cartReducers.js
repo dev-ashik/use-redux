0461b183d0a9f94aef4fdd89e41b11ea74d733b3
dev-ashik <61373940+dev-ashik@users.noreply.github.com>
2021-10-09 13:39:16 +0800

    done

---
+++ src/redux/reducers/cartReducers.js
@@ -0,0 +1,37 @@
+// import {ADD_TO_CART, REMOVE_FROM_CART} from "../action/cartActions";
+import {ADD_TO_CART, REMOVE_FROM_CART} from "../action/cartActions";
+
+const initialState = {
+    cart: [],
+    products: [
+    {name: "Lenovo Laptop", id: 1},
+    {name: "Asus Laptop", id: 2},
+    {name: "Dell Laptop", id: 3},
+    {name: "HP Laptop", id: 4},
+    {name: "Toshiba Laptop", id: 5}
+]
+}
+const cartReducers = (state = initialState, action) =>{
+    switch(action.type){
+        case ADD_TO_CART:
+            console.log(action);
+            // const newId = action.id;
+            const newItem = {
+                productId: action.id, 
+                name: action.name,
+                cartId: state.cart.length + 1
+            }
+            const newCart = [...state.cart, newItem];
+            return {...state, cart: newCart};
+
+        case REMOVE_FROM_CART:
+            const cartId = action.cartId;
+            const remainingCart = state.cart.filter(item => item.cartId !== cartId);
+            return {...state, cart: remainingCart};
+
+        default:
+            return state;
+    }
+}
+
+export default cartReducers;
